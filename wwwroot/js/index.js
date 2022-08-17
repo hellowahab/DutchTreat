e1128807a7c4bb9a717bc16aae6d048d10b180b0
hellowahab <hellowahab@gmail.com>
2022-08-17 23:42:59 +0500

    Getting started with Javascript

---
+++ wwwroot/js/index.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    console.log("First log in Console");
+    var theForm = $("#theForm");
+    //theForm.hide();
+    /*
+    var button = document.getElementById("buyButton");
+    button.addEventListener("click", function () {
+        console.log("Buying Item");
+    });
+    */
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("Click on the item " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(1000);
+    });
+});
+
